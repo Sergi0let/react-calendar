fc6172d67fdc38a241a279b821a8974865e99f4c
Sergi0let <s.vashkevych@gmail.com>
2022-08-21 20:33:35 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
 
@@ -6,22 +6,35 @@ import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 
 import './common.scss';
 
-class App extends Component {
-  state = {
-    weekStartDate: new Date(),
-  };
+const App = () => {
+  const [weekStartDate, setWeekStartDate] = useState(new Date());
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-
-    return (
-      <>
-        <Header />
-        <Calendar weekDates={weekDates} />
-      </>
-    );
-  }
-}
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  return (
+    <>
+      <Header />
+      <Calendar weekDates={weekDates} />
+    </>
+  );
+};
 
 export default App;
+
+//--------------------------------------
+// class App extends Component {
+//   state = {
+//     weekStartDate: new Date(),
+//   };
+
+//   render() {
+//     const { weekStartDate } = this.state;
+//     const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+
+//     return (
+//       <>
+//         <Header />
+//         <Calendar weekDates={weekDates} />
+//       </>
+//     );
+//   }
+// }
--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Navigation from './../navigation/Navigation';
 import Week from '../week/Week';
@@ -7,26 +7,43 @@ import events from '../../gateway/events';
 
 import './calendar.scss';
 
-class Calendar extends Component {
-  state = {
-    events,
-  };
-
-  render() {
-    const { weekDates } = this.props;
-
-    return (
-      <section className="calendar">
-        <Navigation weekDates={weekDates} />
-        <div className="calendar__body">
-          <div className="calendar__week-container">
-            <Sidebar />
-            <Week weekDates={weekDates} events={this.state.events} />
-          </div>
+const Calendar = ({ weekDates }) => {
+  const [event, setEvent] = useState(events);
+  return (
+    <section className="calendar">
+      <Navigation weekDates={weekDates} />
+      <div className="calendar__body">
+        <div className="calendar__week-container">
+          <Sidebar />
+          <Week weekDates={weekDates} events={event} />
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default Calendar;
+
+// ------------------------------
+
+// class Calendar extends Component {
+//   state = {
+//     events,
+//   };
+
+//   render() {
+//     const { weekDates } = this.props;
+
+//     return (
+//       <section className="calendar">
+//         <Navigation weekDates={weekDates} />
+//         <div className="calendar__body">
+//           <div className="calendar__week-container">
+//             <Sidebar />
+//             <Week weekDates={weekDates} events={this.state.events} />
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
